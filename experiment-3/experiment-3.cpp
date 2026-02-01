80ee17a7476a64aa95ff55c18644de38a425c6a4
Pahul1105 <11pahulsingh@gmail.com>
2026-02-01 20:29:17 +0530

    Update experiment-3.cpp

--- experiment-3/experiment-3.cpp
+++ experiment-3/experiment-3.cpp
@@ -1,1 +1,32 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    unordered_map<int, int> mp;
+    mp[0]=-1;
+    int sum=0;
+    int max_length=0;   
+    for(int i=0;i<n;i++){
+        char status;
+        cin>>status;
+        
+        if(status=='P'){
+            sum+=1;
+        } 
+        else if(status=='A'){
+            sum-=1;
+        }
+        if(mp.find(sum)!=mp.end()){
+            int length=i-mp[sum];
+            if(length>max_length) {
+                max_length=length;
+            }
+        } 
+        else{
+            mp.insert({sum, i});
+        }
+    }
+    cout<<max_length<<endl;
+    return 0;
+}
