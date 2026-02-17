474e501e92f0f1ee7622798c075cfd8ab1fc8c75
Pahul1105 <11pahulsingh@gmail.com>
2026-02-17 22:59:02 +0530

    Update kokoeatingbananas.cpp

--- Session-18/Problem-875/kokoeatingbananas.cpp
+++ Session-18/Problem-875/kokoeatingbananas.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    bool possible(vector<int>& piles,int h, int mid) {
+        long long cnt=0;
+        for(int i=0;i<piles.size();i++) {
+            cnt+=piles[i]/mid;
+            if (piles[i]%mid!=0)
+                cnt++;    
+        }
+        return cnt <= h;
+    }
+    int minEatingSpeed(vector<int>& piles,int h) {
+        int low=1;
+        int high=*max_element(piles.begin(), piles.end());
+        while (low<high) {
+            int mid=low+(high-low)/2;
+            if (possible(piles,h,mid))
+                high=mid;     
+            else
+                low=mid+1;  
+        }
+        return low;
+    }
+};
