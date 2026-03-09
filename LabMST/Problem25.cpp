28f6195d4b4a809982a717fc6dd2a21fd9db5a81
Pahul1105 <11pahulsingh@gmail.com>
2026-03-09 15:44:51 +0530

    Update Problem25.cpp

--- LabMST/Problem25.cpp
+++ LabMST/Problem25.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* start=head;
+        ListNode* end=head;
+        for(int i=0;i<k;i++){
+            if(!end) return head;
+            end=end->next;
+        }
+        ListNode* prev=NULL;
+        ListNode* curr=start;
+        while(curr!=end){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        start->next = reverseKGroup(end, k);
+        return prev;
+    }
+};
